fb4cf93721904a659a41ebc611365340185679c5
ushajayarama <80263708+ushajayarama@users.noreply.github.com>
2021-05-28 16:13:24 +0530

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,10 @@
+class Slingshot{
+    constructor(bodyA,pointB){
+        var optionns = {
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness:0.04,
+            length:10
+        }
+    }
+}
